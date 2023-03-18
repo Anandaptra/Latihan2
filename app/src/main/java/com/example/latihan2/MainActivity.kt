b9f2a4bd8b3d3042584363e5d4772b81d200bac6
BAGAS <anndptra658@gmail.com>
2023-03-19 00:51:51 +0700

    Latihan

--- app/src/main/java/com/example/latihan2/MainActivity.kt
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.halaman_register)
+        setContentView(R.layout.halaman_login)
     }
 }
